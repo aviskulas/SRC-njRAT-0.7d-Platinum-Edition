5e2718221f2d6066adbec3e19a7ebd8525675245
ChimesOfDestruction <127018596+ChimesOfDestruction@users.noreply.github.com>
2023-03-07 19:52:58 +0000

    Add files via upload

---
+++ Stub/STUB/frmSustos.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Imports System.Deployment
+Imports System.Diagnostics
+Imports System.Windows.Forms
+Imports System.Timers
+
+
+Public Class frmSustos
+    Private Sub frmSustos_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Try
+            Dim PARADA As New Stopwatch
+            PARADA.Start()
+            Do While PARADA.ElapsedMilliseconds < 3000
+                Application.DoEvents()
+            Loop
+            PARADA.Stop()
+            Me.Close()
+        Catch ex As Exception
+        Finally
+            Me.Close()
+        End Try
+    End Sub
+End Class
